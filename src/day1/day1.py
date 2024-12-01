ede2225895de443a9eeacfc79b19d7debb0d1c86
BoBThegrEat297 <nathanthompson256@outlook.com>
2024-12-01 05:11:13 +0000

    huh

--- src/day1/day1.py
+++ src/day1/day1.py
@@ -1,1 +1,10 @@
-print("Hello World")
+import sys
+
+def main():
+    data = open("input1.txt", "r")
+    # data.sys.stdin.read().strip()
+
+    print(data)
+
+if __name__ == "__main__":
+    main()

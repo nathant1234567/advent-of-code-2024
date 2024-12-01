1c373190e2e3f6d1b7d4b2b53da70b87b714b682
BoBThegrEat297 <nathanthompson256@outlook.com>
2024-12-01 06:05:48 +0000

    day1

---
+++ src/day1/part1.py
@@ -0,0 +1,27 @@
+
+def main():
+
+    sum = 0
+    with open("test.txt") as file:
+        list1 = []
+        list2 = []
+        for line in file:
+            parts = line.split()
+            if len(parts) >= 2:  
+                list1.append(int(parts[0])) 
+                list2.append(int(parts[1]))  
+
+        list1.sort()
+        list2.sort()
+
+        for i in range(len(list1)):
+            temp = abs((int(list2[i])) - (int(list1[i])))
+            sum += temp
+            
+        print(sum)
+            
+        
+        
+    
+if __name__ == "__main__":
+    main()
---
+++ src/day1/part2.py
@@ -0,0 +1,25 @@
+from collections import Counter
+
+def main():
+
+    sum = 0
+    with open("test.txt") as file:
+        list1 = []
+        list2 = []
+        for line in file:
+            parts = line.split()
+            if len(parts) >= 2:  
+                list1.append(int(parts[0])) 
+                list2.append(int(parts[1])) 
+
+        counts = Counter(list2)
+
+        for num in list1:
+            sum += num * counts.get(num, 0)
+            
+        print(counts)
+        
+        
+    
+if __name__ == "__main__":
+    main()

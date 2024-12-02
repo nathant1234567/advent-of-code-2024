efb30e745b9deec02a796f00bbdff06738037f79
NathanT <50116108+nathant1234567@users.noreply.github.com>
2024-12-02 12:02:53 +0000

    Update part1.py

--- src/day2/part1.py
+++ src/day2/part1.py
@@ -4,7 +4,7 @@ def main():
     with open("day2-data.txt") as file:
         for line in file:
             # convert the line into a list of integers
-            levels = list(map(int, line.split()))
+            levels = list(map(int, line.split())) # line.split is putting it in a list, map is converting them to strings, list is putting them back into a list.
 
             # calculate the differences between consecutive levels
             differences = [levels[i + 1] - levels[i] for i in range(len(levels) - 1)]
